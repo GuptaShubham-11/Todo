31149e104679fed797a2d4a0c678daeabea43e6a
GuptaShubham-11 <shu919847@gmail.com>
2025-01-13 20:17:53 +0530

    add registerUser endpoint

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,53 @@
+import { User } from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import ApiError from "../utils/ApiError.js";
+import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudinary.js";
+
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, username, password } = req.body;
+
+    if (!name || !username || !password) {
+        throw new ApiError(400, "Please fill all the fields");
+    }
+
+    if (name.trim() === "" || username.trim() === "" || password.trim() === "") {
+        throw new ApiError(400, "Please fill all the fields");
+    }
+
+    const existedUser = await User.findOne({ username });
+
+    if (existedUser) {
+        throw new ApiError(400, "Username already exists");
+    }
+
+    const profilePicLocalPath = req.file?.path;
+
+    if (!profilePicLocalPath) {
+        throw new ApiError(400, "Profile picture is required");
+    }
+
+    const profilePic = await uploadOnCloudinary(profilePicLocalPath);
+
+    const user = await User.create({
+        name,
+        username: username.toLowerCase(),
+        password,
+        profilePic: profilePic.url
+    });
+
+    if (!user) {
+        throw new ApiError(400, "User creation failed !!");
+    }
+
+    return res
+        .status(201)
+        .json(
+            new ApiResponse(
+                201,
+                user,
+                "User created successfully !!"
+            )
+        );
+
+});
