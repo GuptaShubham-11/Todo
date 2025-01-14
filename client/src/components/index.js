795a59bb11e11aa354310f0120dc1fc2d044e87f
GuptaShubham-11 <shu919847@gmail.com>
2025-01-14 20:21:07 +0530

    add imports & exports for easy to access

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Layout from "./Layout.jsx";
+import ThemeToggle from "./ThemeToggle.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+export {
+    Layout,
+    ThemeToggle,
+    Header,
+    Footer
+};
---
+++ client/src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
+import NotFound from "./NotFound.jsx";
+
+export {
+    Home,
+    Login,
+    Register,
+    NotFound
+};
