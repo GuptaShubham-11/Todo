46c6eeb1f953880ee7e9e4b3eb60c213c10ed57e
GuptaShubham-11 <shu919847@gmail.com>
2025-01-15 19:06:04 +0530

    add useEffect for theme changer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import { Layout } from "./components";
 import { Home, Login, Register, NotFound } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const theme = useSelector((state) => state.theme.theme);
+
+  useEffect(() => {
+    // Apply the theme class to the <html> element
+    const root = document.documentElement;
+    root.classList.remove("light", "dark"); // Remove any existing theme classes
+    root.classList.add(theme); // Add the current theme class
+  }, [theme]); // Runs whenever the theme changes
+
+
   return (
     <>
       <Router>
