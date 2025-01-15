82109d533cd2def2f25b126b325e3815c0c93f6a
GuptaShubham-11 <shu919847@gmail.com>
2025-01-15 19:05:11 +0530

    add imports to easy exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,18 @@ import Layout from "./Layout.jsx";
 import ThemeToggle from "./ThemeToggle.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import InputField from "./InputField.jsx";
+import Button from "./Button.jsx";
+import Alert from "./Alert.jsx";
+import Loader from "./Loader.jsx";
 
 export {
     Layout,
     ThemeToggle,
     Header,
-    Footer
+    Footer,
+    InputField,
+    Button,
+    Alert,
+    Loader
 };
