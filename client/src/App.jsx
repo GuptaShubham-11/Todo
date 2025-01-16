998ed6d98e26c76bcbc4f83adbacdeda99543d97
GuptaShubham-11 <shu919847@gmail.com>
2025-01-16 09:46:20 +0530

    add routes changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
-
   const theme = useSelector((state) => state.theme.theme);
 
   useEffect(() => {
@@ -15,21 +14,19 @@ function App() {
     root.classList.add(theme); // Add the current theme class
   }, [theme]); // Runs whenever the theme changes
 
-
   return (
-    <>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Layout wraps all routes */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} /> {/* Default route */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<NotFound />} /> {/* Fallback for undefined routes */}
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
