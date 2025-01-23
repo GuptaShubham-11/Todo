529ecd6d97004f51fa1abbcab9a7b6c55666d017
GuptaShubham-11 <shu919847@gmail.com>
2025-01-23 20:40:41 +0530

    fix: update API proxy configuration to use production URL with fallback to localhost

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://todo-to90.onrender.com' || 'http://localhost:3000'
     }
   },
   plugins: [react()],
