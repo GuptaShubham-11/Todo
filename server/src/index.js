0ab432233c3ab6ffa9b146b2dbd16ac086f40745
GuptaShubham-11 <shu919847@gmail.com>
2025-01-24 14:46:16 +0530

    feat: move server and home route endpoints to index.js for better organization

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,12 @@ connectDB()
         app.on("error", (error) => {
             throw error;
         });
+        app.get("/server", (req, res) => {
+            return res.status(200).send("Server is running");
+        });
+        app.get("/", (req, res) => {
+            return res.status(200).send("Home page");
+        });
         app.listen(process.env.PORT || 3000, () => {
             console.log(`http://localhost:${process.env.PORT}`);
         });
