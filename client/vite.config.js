0c97e3f8224995d8d79a0065a19592857d3d8264
GuptaShubham-11 <shu919847@gmail.com>
2025-01-25 09:11:51 +0530

    fix: set base path for Vite configuration to support relative URLs

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     proxy: {
       '/api': process.env.VITE_SERVER_API_URL || 'http://localhost:3000',
