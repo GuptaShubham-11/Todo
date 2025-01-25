2065a13c12964813918abc3b39b494469099fe57
GuptaShubham-11 <shu919847@gmail.com>
2025-01-25 12:11:32 +0530

    fix: update API proxy URL in Vite configuration to use production server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: './',
   server: {
     proxy: {
-      '/api': process.env.VITE_SERVER_API_URL || 'http://localhost:3000',
+      '/api': process.env.VITE_SERVER_API_URL || 'https://todo-nf4y.onrender.com',
     },
   },
   plugins: [react()],
